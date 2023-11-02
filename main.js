74d39a5e255df17135377e421f86edb9caa40628
haydnkerr <haydn.kerr@uconn.edu>
2023-11-02 14:29:13 -0400

    changed import url

--- main.js
+++ main.js
@@ -168,8 +168,8 @@ function moonInteraction() {
 
 let isAnimationRunning = true;
 
-let al = new THREE.AmbientLight(0xffffff, 0.07);
-scene.add(al)
+// let al = new THREE.AmbientLight(0xffffff, 0.2);
+// scene.add(al)
 
     let directionalLight = new THREE.DirectionalLight(0xffffff, 5); // Color, Intensity
 directionalLight.position.set(1, 1, 1); // Set the direction
