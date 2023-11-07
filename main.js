acc264e645d87bfc615303d39ec9ec5ccbb170c5
haydnkerr <haydn.kerr@uconn.edu>
2023-11-07 11:16:39 -0500

    code cleanup
    code cleanup

--- main.js
+++ main.js
@@ -16,49 +16,8 @@ let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
 
 
-// const earthTexture = new 
-// THREE.TextureLoader().load('./assets/earth-sphere.png');
-
-// const geometry = new THREE.SphereGeometry( 3, 64, 64 ); 
-// const material = new THREE.MeshBasicMaterial( {map: earthTexture,} ); 
-// const sphere = new THREE.Mesh( geometry, material ); 
-
-// scene.add( sphere );
-
-// camera.position.z = 20;
-
-
-
-
-
-
-// function animate() {
-// 	requestAnimationFrame( animate );
-
-// 	cube.rotation.x += 0.0;
-// 	cube.rotation.y += 0.01;
-
-// 	renderer.render( scene, camera );
-// }
-
-// animate();
-
-// loader.load( './assets/mars-real.glb', function ( gltf ) {
-// 	renderer.render( scene, camera )
-//     scene.add(gltf.scene);
-
-
-// }, undefined, function ( error ) {
-
-// 	console.error( error );
-
-// } );
 
 // Instantiate a loader
 let loader = new GLTFLoader();
@@ -242,7 +201,7 @@ function populatePlanet() {
 
     let planetImageContainerReal = document.getElementById('planet-image-container')
     let renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000, 0); // the default
+    renderer.setClearColor(0x000000, 0); 
     renderer.setSize(window.innerWidth, window.innerHeight);
     planetImageContainerReal.appendChild(renderer.domElement);
     camera.position.z = 2.5;
@@ -281,156 +240,5 @@ function populatePlanet() {
 
 
 
-
-
-
-// Load a glTF resource
-// loader.load(
-// 	// resource URL
-// 	marsImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		mars = gltf.scene
-// 		scene.add( mars );
-// 		mars.position.x = 20
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-// Load a glTF resource Venus
-// loader.load(
-// 	// resource URL
-// 	venusImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		venus = gltf.scene
-// 		scene.add( venus );
-// 		venus.position.x = -10
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-// Load a glTF resource Mercury
-// loader.load(
-// 	// resource URL
-// 	mercuryImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		mercury = gltf.scene
-// 		scene.add( mercury );
-// 		mercury.position.x = -5
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-// loader.load(
-// 	// resource URL
-// 	neptuneImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		neptune = gltf.scene
-// 		scene.add( neptune );
-// 		neptune.position.x = 10
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-// Load a glTF resource
-// loader.load(
-// 	// resource URL
-// 	earthImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		earth = gltf.scene
-// 		scene.add( earth );
-
-// 		console.log(earth);
-// 		earth.position.x = -15
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-// loader.load(
-// 	// resource URL
-// 	sunImage,
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		sun = gltf.scene
-// 		scene.add( sun );
-
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
 
 
